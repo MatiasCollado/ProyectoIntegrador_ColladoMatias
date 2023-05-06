d533810f6cf0ca4598b411d08f07722986e65f81
MatiasCollado <matiasaccm@gmail.com>
2023-05-05 21:04:14 -0300

    Fin Bootstrap

---
+++ FrontEnd/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { LogoApComponent } from './component/logo-ap/logo-ap.component';
+import { BannerComponent } from './component/banner/banner.component';
+import { AcercaDeComponent } from './component/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './component/experiencia/experiencia.component';
+import { EducacionComponent } from './component/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HysComponent } from './component/hys/hys.component';
+import { ProyectoComponent } from './component/proyecto/proyecto.component';
+import { FooterComponent } from './component/footer/footer.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LogoApComponent,
+    BannerComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HysComponent,
+    ProyectoComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
